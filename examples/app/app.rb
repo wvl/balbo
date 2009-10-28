07f854dcadb283103dd572247b9e53ad78967a51
Wayne Larsen <wayne@larsen.st>
2009-10-28 08:33:07 -0600

    Add View module integration, and context method for adding to the lookup stack

--- examples/app/app.rb
+++ examples/app/app.rb
@@ -1,21 +1,33 @@
 require 'balbo/sinatra'
 
 class App < Sinatra::Base
+  register Balbo::Sinatra
   dir = File.dirname(File.expand_path(__FILE__))
   set :views,     dir
   set :public,    "#{dir}/public"
   set :static,    true
   
-  User = Struct.new(:name, :email)
-  
-  Balbo.lookup({'site'=>{'title'=>"The Balbo"}})
+  before do
+    context({'site'=>{'title'=>"The Balbo"}})
+  end
   
   get '/' do
     balbo :index
   end
+
+  User = Struct.new(:name, :email, :admin)
   
   get '/profile' do
-    balbo :profile, User.new('Joe', 'joe@example.com')
+    context({'site'=>{'title'=>'Title Takeover!'}})
+    balbo :profile, User.new('Joe', 'joe@example.com', false)
+  end
+  
+  # The views module is responsible for modifying the data sent to the template if needed.
+  module Views
+    def profile(user)
+      user.admin = true
+      user
+    end
   end
 end
 
--- lib/balbo/sinatra.rb
+++ lib/balbo/sinatra.rb
@@ -8,18 +8,43 @@ module Balbo
     end
     
     def evaluate(scope, locals={}, &block)
-      @engine.render(Context.new(locals))
+      @engine.render(locals[:context])
     end
   end
+
+  module Sinatra
+    module Templates
+      #
+      # Call this in your sinatra app to render a balbo template
+      def balbo(template, locals=nil)
+        Balbo.template_path = settings.views
+        if options.namespace.const_defined?(:Views)
+          mod = options.namespace::Views
+          instance = Class.new
+          instance.extend(mod)
+          locals = instance.send(template, locals) if instance.respond_to?(template)
+        end
+        render :balbo, template, {}, {:context=>context(locals)}
+      end
+
+      # Add data to the lookup context
+      def context(newlocals=nil)
+        @balbo_context ||= Balbo::Context.new
+        @balbo_context.push(newlocals) unless newlocals.nil?
+        @balbo_context
+      end
+    end
+    
+    def self.registered(app)
+      app.helpers Balbo::Sinatra::Templates
+      app.set :namespace, app
+    end
+  end
+
 end
 Tilt.register 'balbo', Balbo::BalboTemplate
+# Sinatra::Base.helpers Balbo::Sinatra::Templates
+# Sinatra::Base.register Balbo::Sinatra
 
-module Sinatra
-  module Templates
-    def balbo(template, locals={})
-      Balbo.template_path = settings.views
-      render :balbo, template, {}, locals
-    end
-  end
-end
+
 
